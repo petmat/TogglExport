5c3c16d2db7eb1817bcc57bcb2e98d631ca0a5e8
Matti Petrelius <matti.petrelius@gmail.com>
2017-11-14 08:50:42 +0200

    Add initial project files

---
+++ TogglExport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TogglExport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
