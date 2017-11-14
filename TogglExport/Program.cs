5b4173fdeab902ebbcf261ff5a16da733dfe7aa5
Matti Petrelius <matti.petrelius@gmail.com>
2017-11-14 09:24:04 +0200

    Add editorconfig to change tabs to spaces

--- TogglExport/Program.cs
+++ TogglExport/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 
 namespace TogglExport {
-	class Program {
-		static void Main(string[] args) {
-			Console.WriteLine("Example usage: toggl-export --api-key xxxx");
-		}
-	}
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Example usage: toggl-export --api-key xxxx");
+        }
+    }
 }
