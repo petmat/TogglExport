773a7820fb7ad7c0e6e1a3695b93d22fbe772878
Matti Petrelius <matti.petrelius@gmail.com>
2017-11-14 11:08:08 +0200

    Add RestSharp for fetching time entries

--- TogglExport/TogglExport.cs
+++ TogglExport/TogglExport.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace TogglExport {
     public static class TogglExport {
@@ -13,8 +16,23 @@ namespace TogglExport {
             var timeEntries = FetchTimeEntries(parameters.ApiKey);
         }
 
-        private static IList<TimeEntry> FetchTimeEntries(string apiKey) {
-            return new List<TimeEntry>();
+        private static async Task<IList<TimeEntry>> FetchTimeEntries(string apiKey) {
+            var client = new RestClient("https://www.toggl.com/api/v8") {
+                Authenticator = new HttpBasicAuthenticator(apiKey, "api_token")
+            };
+
+            var request = new RestRequest("time_entries");
+            request.RequestFormat = DataFormat.Json;
+            request.AddParameter("start_date", DateTimeToIso(new DateTime(2017, 11, 1)));
+            request.AddParameter("start_date", DateTimeToIso(new DateTime(2017, 11, 30)));
+
+            var response = await client.ExecuteTaskAsync<List<TimeEntry>>(request);
+
+            return response.Data;
+        }
+
+        private static string DateTimeToIso(DateTime value) {
+            return value.ToString("yyyy-MM-ddTHH\\:mm\\:ss.fffffffzzz");
         }
     }
 }
