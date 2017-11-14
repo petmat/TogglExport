fb78073a59943cc7e7e5c281c4ef1b661719f83a
Matti Petrelius <matti.petrelius@gmail.com>
2017-11-14 09:51:42 +0200

    Add validating and parsing parameters

--- TogglExport/Parameters.cs
+++ TogglExport/Parameters.cs
@@ -1,11 +1,22 @@
-﻿namespace TogglExport {
+﻿using System;
+
+namespace TogglExport {
     public class Parameters {
         public bool Valid { get; private set; }
+        public string ApiKey { get; set; }
+
+        public static readonly Parameters InvalidParameters = new Parameters();
 
         public static Parameters FromArgs(string[] args) {
-            return new Parameters {
-                Valid = false
-            };
+            return ValidateArgs(args) ? ParseArgs(args) : InvalidParameters;
+        }
+
+        private static bool ValidateArgs(string[] args) {
+            return args.Length == 2 && args[0] == "--api-key";
+        }
+
+        private static Parameters ParseArgs(string[] args) {
+            return new Parameters { Valid = true, ApiKey = args[1] };
         }
     }
 }
